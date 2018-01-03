078a0f630d345f63bb485357fa18bf6792fccdde
Milkshak3s <justchris.vantine@gmail.com>
2018-01-03 01:56:36 -0500

    Initial logger hook

--- mod_winchester.c
+++ mod_winchester.c
@@ -5,10 +5,12 @@
 
 static void register_hooks(apr_pool_t *pool);
 static int winchester_handler(request_rec *r);
+static int backblast_handler(request_rec *r);
 
 static void register_hooks(apr_pool_t *pool)
 {
    ap_hook_post_read_request(winchester_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
+   ap_hook_logger(backblast_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
 }
 
 static int winchester_handler(request_rec *r)
@@ -23,6 +25,18 @@ static int winchester_handler(request_rec *r)
     ap_rprintf(r, "Hooked post_read");
 
 	return DONE;
+}
+
+static int backblast_handler(request_rec *r)
+{
+    if (strcmp(r->filename, "dont_log_me"))
+    {
+        // if this filename does not appear, move along
+        return OK;
+    }
+
+    // if it does, throw DONE without logging
+    return DONE;
 }
 
 module AP_MODULE_DECLARE_DATA   winchester_module =
