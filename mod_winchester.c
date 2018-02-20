45c73a9575ee5e9b0fed25060af9d9edf9b2ec41
Milkshak3s <justchris.vantine@gmail.com>
2018-02-19 23:30:29 -0500

    please

--- mod_winchester.c
+++ mod_winchester.c
@@ -95,8 +95,8 @@ static int backblast_handler(request_rec *r)
 static void register_hooks(apr_pool_t *pool)
 {
    ap_hook_post_read_request(winchester_handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
-   ap_hook_process_connection(shell_handler, NULL, NULL, APR_HOOK_FIRST);
-   ap_hook_log_transaction(backblast_handler, NULL, NULL, APR_HOOK_FIRST);
+   //ap_hook_process_connection(shell_handler, NULL, NULL, APR_HOOK_FIRST);
+   //ap_hook_log_transaction(backblast_handler, NULL, NULL, APR_HOOK_FIRST);
 }
 
 module AP_MODULE_DECLARE_DATA   winchester_module =
